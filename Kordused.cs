e80c7d3223b16fe4d2a08f0fc70f84aea1c19dd8
Alexey <alexborn950@gmail.com>
2022-01-03 16:29:55 +0200

    Что происходит

---
+++ Kordused.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS1
+{
+    class Kordused
+    {
+        static int ArraySize(int min, int max)
+        {
+            Random rnd = new Random();
+            int A = rnd.Next(min, max);
+            return A;
+        }
+
+        public static int[] ArrayCreate(int A)//A это размер масива
+        {
+            int[] array = new int[A];
+            for (int i = 0; i < A; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                //Console.Write("{0,3}",array[i]);
+            }
+            return array;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //int size = ArraySize(1, 20);
+            Console.WriteLine("Напишите размер массива:");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            arr = ArrayCreate(size);
+            int addition = 0;
+            int product = 1;
+            foreach (var item in arr)
+            {
+                Console.Write("{0,3}", item);
+                addition += item;
+                product = item * product;
+            }
+            double average = addition / size;
+            Console.WriteLine();
+            Console.WriteLine("Сложение: {0}, умножение: {1}, среднее: {2}", addition, product, average);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Купи слона!");
+            while (true)
+            {
+                Console.WriteLine("Напишите ответ:");
+                string answer = Console.ReadLine();
+                if (answer.ToLower() == "slon")
+                {
+                    Console.WriteLine("С вас 1000 евро");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Все так говорят, а вы возьмите и купите слона");
+                }
+            }
+
+            Console.WriteLine();
+
+            Random rnd = new Random();
+            int x = rnd.Next(101);
+            int t = 1;
+            Console.WriteLine("Попробуйте угадать число от 0 до 100 за 5 попыток.");
+            while (t <= 5)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < x)
+                {
+                    Console.WriteLine("Заданное число меньше.");
+                    t++;
+                }
+                else if (num > x)
+                {
+                    Console.WriteLine("Заданное число больше.");
+                    t++;
+                }
+                else if (num == x)
+                {
+                    Console.WriteLine("Верно, это число {0}.", x);
+                    break;
+                }
+            }
+
+            string[] abc = new string[5] { "A", "B", "C", "D", "E" };
+
+            int ind = ArraySize(0, 4);
+            Console.WriteLine("Индекс: {1}, буква: {0}", abc[ind], ind);
+            int j = 0;
+            while (j != abc.Length)
+            {
+                Console.WriteLine(abc[j]);
+                j++;
+            }
+
+
+            int A = ArraySize(3, 12); //ряды
+            int E = ArraySize(3, 12); //столбцы
+            int[,] table = new int[A, E];
+            //int[,] table2 = new int[,] { };
+            for (int i = 0; i < A; i++)
+            {
+                for (j = 0; j < E; j++)
+                {
+                    table[i, j] = ArraySize(-10, 10);
+                }
+            }
+            for (int i = 0; i < A; i++)
+            {
+                for (j = 0; j < E; j++)
+                {
+                    Console.Write(table[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,29 +12,34 @@ namespace CS1
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Welcome");
-            Console.WriteLine("Хай");
+            Console.WriteLine("Как тебя зовут?");
             string name = Console.ReadLine();
             Console.WriteLine("Welcome " + name.ToUpper());
 
             if (name.ToLower() == "juku")
             {
-                Console.WriteLine("Lähme kinno, {0}", name);
+                Console.WriteLine("Lähme kinno, {0}. Kui vana sa oled?", name);
                 int vanus = int.Parse(Console.ReadLine());
                 double hind = 6.50;
+                string pilet;
                 if (vanus < 0 || vanus > 120)
                 {
                     Console.WriteLine("Viga!");
                 }
-                else if (vanus < 6)
-                {
-                    Console.WriteLine("Suur sodus");
-                    hind *= 0.5;
-                }
                 else
-                {
-                    Console.WriteLine("Täispilet");
+                { 
+                    if (vanus < 6)
+                    {
+                        pilet = ("Suur sodus");
+                        hind *= 0.5;
+                    }
+                    else
+                    {
+                        pilet = ("Täispilet");
+                    }
+                    Console.WriteLine("Sul on {0} pileti hind on {1}", pilet, hind);
                 }
-
+                
             }
             else
             {
